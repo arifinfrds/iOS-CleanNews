2b02c9682862753eadd7ca00f9759c22a1d4f161
Arif Luthfiansyah <lutluthfiansyah@gmail.com>
2019-12-27 13:51:15 +0700

    Preparing package for MVVM with Clean Architecture

---
+++ CleanNews/Infrastructure/Network/NetworkConfig.swift
@@ -0,0 +1,13 @@
+//
+//  Config.swift
+//  CleanNews
+//
+//  Created by Arif Luthfiansyah on 27/12/19.
+//  Copyright © 2019 arifinfrds. All rights reserved.
+//
+
+import Foundation
+
+struct NetworkConfig {
+    
+}
